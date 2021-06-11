47c3d630a49a7da16d5a8ff449bfc8c1bbee1086
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-11 00:35:31 -0400

    in progress

--- generate_markdown.js
+++ generate_markdown.js
@@ -1,33 +1,60 @@
-function generateReadme(response){
-    return `#${response.projectName}
-    
-            ## Description
+function generateLicense(license){
+    if(license === 'MIT'){
+         return "(https://img.shields.io/badge/License-MIT-yellow.svg)"
+    }
+    else if(license === 'APACHE 2.0'){
+        return "(https://img.shields.io/badge/License-Apache%202.0-blue.svg)"
+    }
 
-                ${response.discription}
-    
-    
-            ## Table of Contents
-                - [Installation](#installation)
-                - [Usage](#usage)
-                - [Credits](#credits)
-                - [License](#license)
-
-            ## Installation
-
-                ${response.command}
-
-            ## Usage
-
-                ${response.infoAboutRepo}
-
-            ## Contributing
-                ${response.infoAboutContributing}
-
-            ## Tests
-                ${response.runTestCommand}
-    ` 
+    else if(license === 'GPL 3.0'){
+        return "(https://img.shields.io/badge/License-GPLv3-blue.svg)"
+    }
+    else if(license === 'BSD 3'){
+        return "(https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)"
+    }
+    else{
+        return "none";
+    }
 }
+function generateReadme(response){
+return `#${response.projectName}
 
+# devops
+
+![Github-liscense]
+    ${generateLicense(response.license)}
+
+## Description
+
+    ${response.discription}
+
+
+## Table of Contents
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Credits](#Credits)
+    - [License](#devops)
+
+## Installation
+
+    ${response.command}
+
+## Usage
+
+    ${response.infoAboutRepo}
+
+## Contributing
+    ${response.infoAboutContributing}
+
+## Tests
+    ${response.runTestCommand}
+
+
+## Questions
+    If you have any questions about the repo open an issue or contact me directly at ${response.email_address} 
+    you can find more info of my work at (https://github.com/${response.username})
+`
+}
 module.exports = {
     generateReadme,
 }
