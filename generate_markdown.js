e78882e753beeeb2aa8f00a449422ff795b06752
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-10 23:06:58 -0400

    basic layout completed

--- generate_markdown.js
+++ generate_markdown.js
@@ -6,8 +6,25 @@ function generateReadme(response){
                 ${response.discription}
     
     
-    
-    
+            ## Table of Contents
+                - [Installation](#installation)
+                - [Usage](#usage)
+                - [Credits](#credits)
+                - [License](#license)
+
+            ## Installation
+
+                ${response.command}
+
+            ## Usage
+
+                ${response.infoAboutRepo}
+
+            ## Contributing
+                ${response.infoAboutContributing}
+
+            ## Tests
+                ${response.runTestCommand}
     ` 
 }
 
